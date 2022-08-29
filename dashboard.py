b514b4a2348eabca6a3f0a6a62e0c5f0767312d9
Peter van Doorn <peter@petervandoorn.com>
2022-08-30 00:47:48 +0200

    cleanup

--- dashboard.py
+++ dashboard.py
@@ -61,10 +61,11 @@ if st.checkbox('Show Example response and objects'):
     st.subheader("Available objects")
     st.text("These objects can be detected by the YoloV5 model")
     st.text("person bicycle car motorcycle airplane bus train truck boat traffic light fire hydrant stop sign parking meter bench bird cat dog horse sheep cow elephant bear zebra giraffe backpack umbrella handbag tie suitcase frisbee skis snowboard sports ball kite baseball bat baseball glove skateboard surfboard tennis racket bottle wine glass cup fork knife spoon bowl banana apple sandwich orange broccoli carrot hot dog pizza donut cake chair couch potted plant bed dining table toilet tv laptop mouse remote keyboard cell phone microwave oven toaster sink refrigerator book clock vase scissors teddy bear hair drier toothbrush")
-    st.subheader("Here are 2 example API responses")
+    st.subheader("Here are 2 example API calls and responses")
     st.text("1. Call  /v2/newassignment/200	to get a new object to find")
-    st.code("""{"apple":"🍎"}""")
-    st.text("2. Call /v2/uploadfile/boat to see if a boat is in the picture. It will return:")
+    st.code("""{"apple":"🍎"} \n""")
+    st.text("* Make sure to add a integer as a score, as assignments get harder with higher scores")
+    st.text("2. Call /v2/uploadfile/boat to detect a boat in the picture. It will return:")
     st.code("""
         {
         "Searchedfor:":"boat",\n
